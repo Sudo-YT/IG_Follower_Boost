cb7e155a42d488f032bd31de1444b6c84dc6abd3
Sudo <64551688+Sudo-YT@users.noreply.github.com>
2021-10-30 19:58:59 -0700

    Update main.py

--- main.py
+++ main.py
@@ -25,24 +25,33 @@ print("\033[1;32;40m\n""""{}
 | |_|   ||       ||       | _____| |  |   |  |   |___ |   |  | |                          
 |_______||_______||_______||_______|  |___|  |_______||___|  |_|                          
                                                         
-{}\n\t[+] Welcome To Sudos's Instagram Follower Booster (•◡ •) /\n\t[+] Subscribe To Sudo On YT\n\t[+] NOTE: Make Sure You Have Firefox Installed And 2FA Is Off\n{}""".format("="*100,"="*100,"="*100))
+{}\n\t[+] Welcome To Sudo's Instagram Follower Booster (•◡ •) /\n\t[+] Subscribe To Sudo On YT\n\t[+] NOTE: Make Sure You Have Firefox Installed And 2FA Is Off\n{}""".format("="*100,"="*100,"="*100))
 
 user = input("[+] Enter you username: ")
 pw = getpass.getpass("[+] Enter you user password: ")
-fol = input("[+] Would you like to follow these accounts? (y/n): ")
-follow = input("[+] Enter the max amount of accounts you want to view: ")
+like = input("[+] What tag do you want to search?: ")
+view = input("[+] How many posts do you want to view?: ")
+fol = input("[+] Do you want to follow these accounts? (y/n): ")
+com = input("[+] Do you want to comment on these posts? (y/n): ")
 
 session = InstaPy(username=user, password=pw)
 session.login()
 
-session.set_relationship_bounds(enabled=True, max_followers=int(follow))
+session.set_relationship_bounds(enabled=True, max_followers=int(view))
 
 if fol == "y":
     session.set_do_follow(True, percentage=100)
 if fol == "n":
-    session.set_do_follow(True, percentage=0)
+    session.set_do_follow(False, percentage=0)
 
-session.like_by_tags(["love", "photography", "travel"], amount=4) # Add tags to search here and when you do, add all the tags together and add one to amount
+if com == "y":
+    session.set_do_comment(enabled=True, percentage=100)
+    session.set_comments(["Amazing!", "Nice", "Great stuff"])
+if com == "n":
+    session.set_do_comment(enabled=False, percentage=0)
+
+session.like_by_tags([like], amount=2)
+
 session.set_dont_like(["nsfw", "sex"]) # Add tags to 100% avoid here
 
 session.end()
